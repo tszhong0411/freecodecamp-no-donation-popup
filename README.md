4f8bc5e7e58b0f8ceafe2d0dcff1fdbfd0880d11
tszhong0411 <me@honghong.me>
2024-05-06 23:44:46 +0800

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+<p align="center">
+  <img alt="" src="./assets/header.png">
+</p>
+
+<h1 align="center">
+  FreeCodeCamp No Donation Pop-up
+</h1>
+
+<p align="center">
+  <img alt="" src="https://img.shields.io/github/languages/top/tszhong0411/freecodecamp-no-donation-popup?style=for-the-badge&labelColor=000">
+  <a aria-label="License" href="https://github.com/tszhong0411/freecodecamp-no-donation-popup/blob/main/LICENSE">
+    <img alt="" src="https://img.shields.io/github/license/tszhong0411/freecodecamp-no-donation-popup?style=for-the-badge&labelColor=000">
+  </a>
+</p>
+
+## Description / 描述
+
+This Tampermonkey script removes the donation popup in FreeCodeCamp.
+
+這個 Tampermonkey 腳本可以移除 FreeCodeCamp 中的捐款彈出式視窗。
+
+## License / 許可證
+
+This project is licensed under the MIT License. See the [LICENSE](./LICENSE) file for more information.
+
+本專案使用 MIT License。更多資訊請參閱 [LICENSE](./LICENSE) 檔案。
---
+++ index.user.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name                 FreeCodeCamp No Donation Pop-up
+// @name:zh-TW           FreeCodeCamp 移除捐款彈出式視窗
+// @name:zh-HK           FreeCodeCamp 移除捐款彈出式視窗
+// @name:zh-CN           FreeCodeCamp 移除捐款弹出式视窗
+// @namespace            https://github.com/tszhong0411/freecodecamp-no-donation-popup
+// @description          Remove donation popup in FreeCodeCamp
+// @description:zh-TW    移除 FreeCodeCamp 中的捐款彈出式視窗
+// @description:zh-HK    移除 FreeCodeCamp 中的捐款彈出式視窗
+// @description:zh-CN    移除 FreeCodeCamp 中的捐款弹出式视窗
+// @author               tszhong0411
+// @version              1.0.0
+// @icon                 https://www.google.com/s2/favicons?sz=64&domain=www.freecodecamp.org
+// @match                *://www.freecodecamp.org/learn/*
+// @match                *://www.freecodecamp.org/*/learn/*
+// @license              MIT
+// @grant                none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  // Function to remove the popup elements
+  function removePopup() {
+      // Select the root element
+      const portalRoot = document.getElementById('headlessui-portal-root');
+
+      // Check if the root element exists and contains the donation-modal
+      if (portalRoot && portalRoot.querySelector('.donation-modal')) {
+          // Remove the entire root element
+          portalRoot.remove();
+
+          // Enable scrolling on the root element if it was made inert
+          const root = document.querySelector('#___gatsby');
+          if (root && root.inert) {
+              root.inert = false;
+          }
+      }
+  }
+
+  // Create a new MutationObserver
+  const observer = new MutationObserver(mutationsList => {
+      for (let mutation of mutationsList) {
+          // Check if nodes were added to the DOM
+          if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+              // Run the function to remove the popup
+              removePopup();
+          }
+      }
+  });
+
+  // Start observing changes in the body and its descendants
+  observer.observe(document.body, { childList: true, subtree: true });
+})();
